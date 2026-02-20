7615062d3764a8300fbc59ce1981ab544502d568
Talha Oz <tozcss@gmail.com>
2026-02-19 19:30:12 -0800

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Talha Oz on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnakeGameTests/SnakeGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  SnakeGameTests.swift
+//  SnakeGameTests
+//
+//  Created by Talha Oz on 2/19/26.
+//
+
+import Testing
+
+struct SnakeGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
